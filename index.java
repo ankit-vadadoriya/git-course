ada81bb2724fe61054dc949887c22dec5cfdf5bc
ankit <ankitvadadoriya00@gmail.com>
2024-08-09 09:27:08 +0200

    Create index file

---
+++ index.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class index {
+    // main method
+    public static void main(String[] args)
+    {
+        List<Integer> nums = new ArrayList<>();
+
+// Adding
+nums.add(2);
+nums.add(5);
+nums.add(8);
+
+
+for (int value : nums) {
+    System.out.println(value);
+}
+
+    }
+  }
